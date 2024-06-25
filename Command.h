94656e6e6f8e767cbc84ea804d398a989d00163a
Nguyễn Đặng Minh Duy <mduy88201010@gmail.com>
2024-06-25 21:38:20 +0700

    update

--- Command.h
+++ Command.h
@@ -2,8 +2,9 @@
 
 
 // ----------------------- RUN COMMAND FUNCTIONS -----------------------------
+void runSorting(int a[], int n, char algo[], char mode[], double& runtime, long long& compare);
 // function that runs the first command type:
-void runCommandTypeOne(int argc, char* argv[]);
+void runCommandTypeOne(int argc, char algorithmName[], char fileName[], char outputParameters[]);
 
 // functions that runs the second command type:
 void runCommandTypeTwo(char algo[], int inputSize, char inputOrder[], char outputParameters[]);
